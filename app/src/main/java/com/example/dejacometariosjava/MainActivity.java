c5d8f1e18e6de82a3222d4c4502d4b7615ae62cd
angelramos <angelgonzalez171104@gmail.com>
2025-01-27 08:20:52 -0600

    Cambios e agregar el toast

--- app/src/main/java/com/example/dejacometariosjava/MainActivity.java
+++ app/src/main/java/com/example/dejacometariosjava/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.dejacometariosjava;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText txtEmail;
+    EditText txtNombre;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        txtEmail = findViewById(R.id.emailInput);
+        txtNombre = findViewById(R.id.nombreInput);
+        btnEnviar = findViewById(R.id.enviarButton);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre = txtNombre.getText().toString();
+                String email = txtEmail.getText().toString();
+
+                if (nombre.isEmpty() || email.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Nombre: " + nombre + "\nEmail: " + email,
+                            Toast.LENGTH_LONG).show();
+
+                    txtNombre.setText("");
+                    txtEmail.setText("");
+                }
+            }
         });
     }
 }
